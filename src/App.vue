b9ca9d5a98ed930abdc497f47795fadbd9f666d9
Cinzya <cynti-ebert@web.de>
2020-10-05 13:03:38 +0200

    chapter 3: built simple single file component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    @{{ user.username }} - {{ fullName }}
+    <strong>Followers:</strong>  {{ followers }}
+    <!-- @event = v-on:event directive to listen to DOM events and run some JavaScript when they’re triggered. -->
+    <button @click="followUser" >
+      Follow
+    </button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  // similar to react states
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: '_MitchellRomney',
+        firstName: 'Mitchell',
+        lastName: 'Romney',
+        email: 'mitchellromney@theearththissqure.com',
+        isAdmin: true
+      }
+    }
+  },
+  // watch data and do something when it changes
+  // Statename (newState, oldState)
+  watch: {
+    followers(newFollowerCount, oldFollowerCount) {
+      if (oldFollowerCount < newFollowerCount) {
+        console.log(`${this.user.username} has gained a follower!`)
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  },
+  // create methods here
+  methods: {
+    followUser() {
+      this.followers++
+    }
+  },
+  // lifecycle methods bzw. hooks in Vue
+  mounted() {
+    this.followUser();
   }
 }
 </script>
@@ -22,5 +62,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
 }
 </style>
